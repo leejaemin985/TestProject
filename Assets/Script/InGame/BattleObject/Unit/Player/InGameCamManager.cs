0af9708f8f547de86db73e915d7079469f875208
DESKTOP-29AOU10\USER <leejaemin@illuni.com>
2025-09-15 19:51:15 +0900

    인게임 시네머신 테스트

--- Assets/Script/InGame/BattleObject/Unit/Player/InGameCamManager.cs
+++ Assets/Script/InGame/BattleObject/Unit/Player/InGameCamManager.cs
@@ -1,6 +1,7 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace InGame
@@ -10,20 +11,38 @@ namespace InGame
         [SerializeField] private CinemachineFreeLook freeLookCam;
         [SerializeField] private Transform lookTarget;
 
+        //[SerializeField] private float speed = .2f;
+        //private Transform head;
+
         private Camera cam;
 
         public void Initialize(Camera cam, Transform lookTarget)
         {
             this.cam = cam;
             //this.lookTarget = lookTarget;
+            //head = lookTarget;
+            //Debug.Log($"Test - head: {lookTarget}");
 
             freeLookCam.enabled = true;
-            freeLookCam.LookAt = this.lookTarget;//lookTarget;
-            freeLookCam.Follow = this.lookTarget;//lookTarget;
+            freeLookCam.LookAt = this.lookTarget;
+            freeLookCam.Follow = this.lookTarget;
 
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
+
+        public void ShakeTest(float value)
+        {
+            freeLookCam.AddComponent<CinemachineBasicMultiChannelPerlin>();
+
+        }
+
+        //private void Update()
+        //{
+        //    if (head == null) return;
+        //    lookTarget.position = Vector3.Lerp(lookTarget.position, head.position, speed);
+            
+        //}
 
 
         /// Entry Point에서 락 해제 중
